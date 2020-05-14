ad1eec7acb0f6daf106a7e8e4f88a6d2e809f17d
dan1lov <danilov@vlinc.ru>
2020-05-14 14:49:06 +0700

    Fixed error in Request

--- src/VKHP/Request.php
+++ src/VKHP/Request.php
@@ -16,7 +16,7 @@ class Request
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
             CURLOPT_HTTPHEADER => $headers ?? [],
-            CURLOPT_POST => $fields === null ? false : true,
+            CURLOPT_POST => $fields !== null,
             CURLOPT_POSTFIELDS => is_array($fields) && $headers === null ? http_build_query($fields) : $fields,
         ] + (array) $options;
         curl_setopt_array($ch, $ch_options);
@@ -33,6 +33,10 @@ class Request
         ?array $options = null
     ): object {
         $request = self::make($url, $fields, $headers, $options);
+        if (empty($request)) {
+            throw new \Exception('response is empty, it is impossible execute json_decode');
+        }
+
         return json_decode($request);
     }
 }
